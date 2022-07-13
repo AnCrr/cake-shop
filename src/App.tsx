a8c8c128d29bbb0e6dd602993c6e517f3d4f561b
Ani <an.cr.0999@gmail.com>
2022-07-13 17:50:50 +0300

    structured

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import "./scss/app.scss";
+import Loadable from "react-loadable";
 
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import { Routes, Route } from "react-router-dom";
-import Cart from "./pages/Cart";
+//import Cart from "./pages/Cart";
 import FullCake from "./pages/FullCake";
 import MainLayout from "./layouts/MainLayout";
+
+const Cart = Loadable({
+  loader: () => import(/* webpackChunkName: "Cart" */ "./pages/Cart"),
+  loading: () => <div>Идёт загрузка корзины...</div>,
+});
 
 function App() {
   return (
